131b9869d29cb1e721ff18a61412e812eec48b1b
Silvio Otero <53842922+silviootero@users.noreply.github.com>
2024-09-05 13:54:48 -0500

    Update api.js

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { loadProgram, executeStep } = require('../controllers/cpuController');
+//const { loadProgram, executeStep } = require('../controllers/cpuController');
 
 const router = express.Router();
 
-router.post('/load-program', loadProgram);
-router.post('/execute-step', executeStep);
+//router.post('/load-program', loadProgram);
+//router.post('/execute-step', executeStep);
 
 module.exports = router;
