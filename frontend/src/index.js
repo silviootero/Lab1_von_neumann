23843560fbd1eb1727cbe7939855e50527ca8cca
Cristian David Tamayo Espinosa <118401392+CrissPr@users.noreply.github.com>
2024-09-11 21:51:58 -0500

    Update index.js
    documentation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'; // Importa la librería React para utilizar JSX y componentes.
+import ReactDOM from 'react-dom'; // Importa ReactDOM para manipular el DOM de la página.
+import App from './App'; // Importa el componente principal de la aplicación.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Renderiza el componente principal `App` dentro del DOM. */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // Selecciona el elemento HTML con el id 'root' como punto de montaje para la aplicación.
 );
 
